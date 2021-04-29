28e1347892552181ab424f8de966c3ff7121ff96
Alexey Ternavskiy <oternavskyi@lohika.com>
2021-04-30 00:24:29 +0300

    Add media

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -8,3 +9,7 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('', include('apps.tg_bot.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
